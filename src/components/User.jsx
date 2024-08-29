5a2979a5dfe0295fa852cf93fed2ae190b340ae5
Sebil Alagoz <s.alagoz@outlook.fr>
2024-08-29 17:02:32 +0200

    MAJ du composant User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,26 +1,9 @@
-// export default function User() {
-//     return (
-//         <div className="header">
-//             <h1>
-//                 Welcome back
-//                 <br />
-//                 Tony Jarvis!
-//             </h1>
-//             <button className="edit-button">Edit Name</button>
-//         </div>
-//     );
-// }
-
-// // à modifier avec les informations de connexion ?
-
-import React, { useState } from 'react';
-import EditUser from './EditUser'; // Assurez-vous que le chemin est correct
+import { useState } from 'react';
+import EditUser from './EditUser';
 
 export default function User() {
-    // État pour déterminer si le composant EditUser doit être affiché
     const [isEditing, setIsEditing] = useState(false);
 
-    // Fonction pour basculer l'état d'édition
     const handleEditClick = () => {
         setIsEditing(true);
     };
